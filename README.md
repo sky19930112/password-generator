a72dc53becde2a65d7d4df50d8d0e13ff3b968f6
sky19930112 <107167603+sky19930112@users.noreply.github.com>
2022-07-12 23:14:48 -0500

    Generate password system

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # password-generator
+*I use getElementById to grab passwordEl as ID = "password" element in HTML (Line 5 in JS)
+* Then I use function to create two strings and then drop in a for loop with math function to generate the random password. (Line 9 in JS)
+*Last step is to use a function to trigure the password generate funtion when the Botton is being clicked (Line 24 in JS)
+
+![image one](./picture/Picture.jpg)
+
--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 // Assignment code here
 
 
+
+var passwordEl = document.getElementById("password");
+
+/* Function to generate combination of password */
+
+function password(){
+    var string = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + 
+            'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+      
+    for (let i = 1; i<= 8; i++) {
+        var char = Math.floor(Math.random()
+                    * characters.length + 1);
+          
+        string += characters.charAt(char)
+    }
+      
+    return string;
+}
+
+function result() {
+    passwordEl.innerHTML = password();
+}
+
+/*
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +40,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+*/
